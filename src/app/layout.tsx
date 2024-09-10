e0c2fb898f51499d7bbf16404fa11e1214df1185
[Dayton <2831102052@k12.hi.us>
2024-09-10 09:02:53 -1000

    Yellow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,25 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className="">
+        <div className="w-full">First Layout</div>
+        
+        <button className="btn btn-secondary btn-block">   <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+  </svg>
+        Button</button>
+
+        {children}
+      </body>
     </html>
   );
 }
