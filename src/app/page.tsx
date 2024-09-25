cf459f8d507a203a3767dbee8e24abd916dca20d
[Dayton <2831102052@k12.hi.us>
2024-09-25 08:53:42 -1000

    Added stuff to prevent trolls and changed to Images, went from mock to real

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
+import { db } from "~/server/db"
 
-const mockUrls = [
+/*const mockUrls = [
   "https://utfs.io/f/9a13ba76-f28f-489d-ac34-73ad87bcb692-5cfjin.jpg",
   "https://utfs.io/f/5e6dd6dc-d49e-4a33-b929-0ec1a8f47e17-ifz351.webp",
   "https://utfs.io/f/ea948937-224f-4e04-9ec5-e04d2de54c4c-ilbysz.webp",
@@ -13,12 +14,15 @@ const mockImages = mockUrls.map((url, index) =>({
   id: index + 1,
   url,
 }))
-
-export default function HomePage() {
+*/
+export default async function HomePage() {
+  const images = await db.query.images.findMany()
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) =>(
+
+
+        {images.map((image) =>(
           <div key={image.id} className="w-48">
             <img src={image.url} />
       </div>
