3a877ac444b3eb1a12413af8225fd658a8712b12
[Dayton <2831102052@k12.hi.us>
2024-12-02 08:50:20 -1000

    Fixed queries by spell checking and added new error Unauthorized

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -2,15 +2,25 @@ import "server-only";
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
 
-export async function getMyImages(): Promise<any[] | null> {
-    const user = await (auth());
+type Image = {
+    id: Number;
+    name: string;
+    url: string;
+    userId: string;
+    createdAt: Date;
+    updatedAt: Date | null;
+};
+
+export async function getMyImages(): Promise<Image[] | null> {
+    const user = await(auth());
+
     if (!user.userId) {
-        return null;
+        throw new Error("Unauthorized");
     };
 
-    const images = await db.query.images.findMany({
-        where: (model, { eq }) => eq(model.userId, user.userId),
-        orderBy: (model, { desc }) => desc(model.id),
+        const images = await db.query.images.findMany({
+            where: (model, { eq }) => eq(model.userId, user.userId),
+            orderBy: (model, { desc }) => desc(model.id),
     });
     return images;
 }
